d32de26861888de7f45f57d01e09fac50bc17c82
cryptoshipwright <cryptoshipwright@protonmail.com>
2021-04-13 13:12:49 -0700

    Fix transfer to transferBorrower for IGSDINFT

--- contracts/interfaces/IGSDINFT.sol
+++ contracts/interfaces/IGSDINFT.sol
@@ -44,12 +44,12 @@ interface IGSDINFT is IERC721Enumerable {
 
     /// @notice Changes the current borrower which will receive the GSDI after it is covered. Reverts if sender is not borrower.
     /// @param _receiver New address to set the borrower to.
-    function transfer(address _receiver) external;
+    function transferBorrower(address _receiver) external;
 
     /// @notice Changes the current borrower and calls onTokenTransfer(address,uint256,bytes) on receiver.
     /// @dev See https://github.com/ethereum/EIPs/issues/677
     /// @param _receiver New address to set the borrower to.
-    function transferAndCall(
+    function transferBorrowerAndCall(
         address _receiver,
         uint256 amount,
         bytes calldata data
